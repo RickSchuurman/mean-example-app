508bbfebeb3ded36380b612c9778a7ec8c61d707
rickschuurman <rick.schuurman@malmberg.nl>
2019-04-23 13:51:31 +0200

    first commit

---
+++ build_frontend.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# cleanup docker
+
+docker stop mean-nginx
+docker rm mean-nginx
+docker rmi mean-nginx
+
+# build frontend
+npm run build -- --output-path=./dist/out --configuration production
+docker build -t mean-nginx .
+
+# deploy frontend
+docker run -d -p 80:80 --name mean-nginx mean-nginx
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://54.194.145.161:3000/api'
+};
